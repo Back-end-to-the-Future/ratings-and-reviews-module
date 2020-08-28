4d76bf0a1bdd985d665102fa6cfbfd2b66e16c8e
Honorio <htaveras90@gmail.com>
2020-08-28 17:14:43 -0600

    update: created database, ci before postgresQL db connection

--- server/server.js
+++ server/server.js
@@ -19,6 +19,10 @@ app.get('/rrmodule', (req, res) => {
 });
 
 app.get('/rrmodule/reviews/:product_id/list', (req, res) => {
+  // let {page} = req.query;
+  // let {count} = req.query;
+  console.log('query: ', req.query);
+
   res.status(200).send(dummyData.listReviews);
 });
 
