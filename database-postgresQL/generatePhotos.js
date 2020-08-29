1abd07b68f2b88537d8bc08d27c27e53df512c66
Honorio <htaveras90@gmail.com>
2020-08-29 15:17:21 -0600

    update: fixed ratio of photos to reviews_id

--- database-postgresQL/generatePhotos.js
+++ database-postgresQL/generatePhotos.js
@@ -6,8 +6,8 @@ const fs = require('fs');
 let seedPhotos = 'reviews_id,url,\n';
 
 const generatePhotos = () => {
-  for (let i = 0; i < 10000000; i += 1) {
-    const reviews_id = Math.floor(Math.random() * 1000000) + 1;
+  for (let i = 0; i < 5000000; i += 1) {
+    const reviews_id = Math.floor(Math.random() * 10000000) + 1;
     const url = faker.image.imageUrl();
     seedPhotos = seedPhotos.concat(`${reviews_id},${url}\n`);
   }
