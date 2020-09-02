94fd141434a7d23b208d8850811d5a811dafff2d
Honorio <htaveras90@gmail.com>
2020-09-02 14:23:22 -0600

    update: refactored with helper func

--- server/models/metadata.js
+++ server/models/metadata.js
@@ -2,44 +2,28 @@
 /* eslint-disable camelcase */
 const pool = require('../../database/database-postgresQL/index.js');
 
+const getChar = (productId, cb, col, table) => {
+  const start = Date.now();
+  const queryString = `SELECT ${col} FROM ${table} WHERE product_id = ${productId}`;
+  pool.query(queryString, (err, res) => {
+    const millis = Date.now() - start;
+    const duration = Math.floor(millis / 1000);
+    if (err) {
+      cb(err);
+    }
+    console.log('executed query: ', { duration, rows: res.rowCount });
+    cb(null, res.rows);
+  });
+};
+
 module.exports = {
   getRatings: (product_id, callback) => {
-    const start = Date.now();
-    const queryString = `SELECT rating FROM reviews WHERE product_id = ${product_id}`;
-    pool.query(queryString, (err, res) => {
-      const millis = Date.now() - start;
-      const duration = Math.floor(millis / 1000);
-      if (err) {
-        callback(err);
-      }
-      console.log('executed query: ', { duration, rows: res.rowCount });
-      callback(null, res.rows);
-    });
+    getChar(product_id, callback, 'rating', 'reviews');
   },
   getRecommend: (product_id, callback) => {
-    const start = Date.now();
-    const queryString = `SELECT recommend FROM reviews WHERE product_id = ${product_id}`;
-    pool.query(queryString, (err, res) => {
-      const millis = Date.now() - start;
-      const duration = Math.floor(millis / 1000);
-      if (err) {
-        callback(err);
-      }
-      console.log('executed query: ', { duration, rows: res.rowCount });
-      callback(null, res.rows);
-    });
+    getChar(product_id, callback, 'recommend', 'reviews');
   },
   getCharacteristics: (product_id, callback) => {
-    const start = Date.now();
-    const queryString = `SELECT * FROM characteristics WHERE product_id = ${product_id}`;
-    pool.query(queryString, (err, res) => {
-      const millis = Date.now() - start;
-      const duration = Math.floor(millis / 1000);
-      if (err) {
-        callback(err);
-      }
-      console.log('executed query: ', { duration, rows: res.rowCount });
-      callback(null, res.rows);
-    });
+    getChar(product_id, callback, '*', 'characteristics');
   },
 };
